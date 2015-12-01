3eee2aba42b36b3ee21643ee75935cfd8ba71160
Miroslav Jiřík <mjirik@kky.zcu.cz>
2015-12-01 09:41:13 +0100

    fix new version vtk

--- dicom2fem/vtk2stl.py
+++ dicom2fem/vtk2stl.py
@@ -11,11 +11,18 @@ def vtk2stl(fn_in, fn_out):
     reader.Update()
 
     gfilter = vtk.vtkGeometryFilter()
-    gfilter.SetInput(reader.GetOutput())
+    if vtk.VTK_MAJOR_VERSION <= 5:
+        gfilter.SetInput(reader.GetOutput())
+    else:
+        gfilter.SetInputData(reader.GetOutput())
+        gfilter.Update()
 
     writer = vtk.vtkSTLWriter()
     writer.SetFileName(fn_out)
-    writer.SetInput(gfilter.GetOutput())
+    if vtk.VTK_MAJOR_VERSION <= 5:
+        writer.SetInput(gfilter.GetOutput())
+    else:
+        writer.SetInputData(gfilter.GetOutput())
     writer.Write()
 
 def main():
