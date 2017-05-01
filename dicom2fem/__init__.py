0b6adc1fbba69edff3d2aecaf844cfb0d2df1d99
Miroslav Jirik <miroslav.jirik@gmail.com>
2017-05-01 15:34:50 +0200

    Bump version: 1.0.17 → 1.0.18

--- dicom2fem/__init__.py
+++ dicom2fem/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ['base', 'dicom2fem', 'genfem_base', 'ioutils', 'marching_cubes',
            'mesh', 'meshio', 'seg2fem', 'viewer', 'vtk2stl']
 
-__version__ = "1.0.17"
+__version__ = "1.0.18"
 
 import base, dicom2fem, genfem_base, ioutils, marching_cubes
 import mesh, meshio, seg2fem, viewer, vtk2stl
