2cf0234ce5c3ac4eb1984383e12bd118de66ca33
Miroslav Jirik <miroslav.jirik@gmail.com>
2017-11-30 12:57:20 +0100

    Bump version: 1.0.26 → 1.0.27

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.0.26',
+    version='1.0.27',
     url='https://github.com/vlukes/dicom2fem',
     author='Vladimir Lukes',
     author_email='vlukes@kme.zcu.cz',
