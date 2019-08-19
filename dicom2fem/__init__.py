4af87647aa090c1177478e49f4b88471bb09e7f7
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-08-19 16:26:24 +0200

    Bump version: 1.1.12 → 1.1.13

--- dicom2fem/__init__.py
+++ dicom2fem/__init__.py
@@ -11,7 +11,7 @@ __all__ = [
     "vtk2stl",
 ]
 
-__version__ = "1.1.12"
+__version__ = "1.1.13"
 
 from . import base, dicom2fem, genfem_base, ioutils, marching_cubes
 from . import mesh, meshio, seg2fem, viewer, vtk2stl
